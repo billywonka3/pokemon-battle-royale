b4e091da6b3311f90ae8a42a834d4986f276f84c
Billy Wong <billywonka3@gmail.com>
2019-06-25 08:30:53 -0400

    Add files via upload

--- src/PokemonBR-Game.js
+++ src/PokemonBR-Game.js
@@ -4,17 +4,17 @@ window.onload = function () {
 
   // Instructions Starter Screen
 
+
   // Starter Pokemon Choice
   let player = $("#player")
 
-  $(clickable).click(function() {
-    let choice = $("class= clickable").prop(src="img/");
-    console.log(choice);
-    $(player.children[0]).prop(`src="img/lillipup1.gif`);
-    $("class= clickable").empty; // Remove all other sprites from the board
-    console.log(choice);
+  $(".clickable").click(function() {
+    // this.style.backgroundColor = 'red';
+    let theSource = $(this).children().prop('src');
+    // console.log(theSource);
+    $('#player').children().prop('src', theSource)
+    $(".clickable").empty(); // Remove all other sprites from the board
   });
-  // 
 
   // Player Controls & Movement
   let x = 1;
@@ -22,33 +22,35 @@ window.onload = function () {
 
   document.onkeydown = function(e) {
     switch (e.key) {
-      case "ArrowUp": if (y > 1) {y--; break;}
-        if (y=1) {x--}; // Prevent grid-edge movement
-      case "ArrowRight": if (x < 12) {x++; break;}
-        if (x=12) {y--}; // Prevent grid-edge movement
-      case "ArrowDown": if (y < 12) {y++; break;}
-        if (y=12) {x++}; // Prevent grid-edge movement
-      case "ArrowLeft": if (x > 1) {x--; break;}
+      case "ArrowUp": if (y > 1) {y--;} break;
+      case "ArrowRight": if (x < 12) {x++;} break;
+      case "ArrowDown": if (y < 12) {y++;} break;
+      case "ArrowLeft": if (x > 1) {x--;} break;
     }
+    console.log(`.r${y}c${x}`)
     $(`.r${y}c${x}`).append(player);
   }
+
+  // Collision Function
+  // Collision() {
+  //   playerHP.value -= 8(); // Use for collision base-damage
+
+  // };
+
 }
-  // $(`.r${y}c${x}`).find('img').remove() // locate player img within coordinates & remove it
-  // $(`.r${y}c${x}`).append(`<img src="img/pichu1.gif"/>`) // move in defined direction and add img to current span
 
 //------------------------------------------------------------------------------
 
-class Game {
-  constructor(){
+// $(`.r${y}c${x}`).find('img').remove() // locate player img within coordinates & remove it
+// $(`.r${y}c${x}`).append(`<img src="img/${pokeCPU}1.gif"/>`) // move in defined direction and add img to current span
+
+
+// class Game {
+//   constructor(){
     // this.health = document.getElementsById('playerHP').value;
     // this.score = document.getElementsById('score').value;
     
-  }
+  // }
   
-  Collision() {
-    playerHP.value -= 8(); // Use for collision base-damage
-
-  }
-}
 
 
--- src/PokemonBR-Pokemon.js
+++ src/PokemonBR-Pokemon.js
@@ -1,3 +1,99 @@
+class Spawn {
+  constructor(){
+    // Random Number Generators
+    let edgeArray = ['top', 'bottom', 'left', 'right']
+    let randomFour = Math.floor(Math.random()*4);
+    let randomTwelve = Math.floor(Math.random()*12);
+    let randomEighteen = Math.floor(Math.random()*18);
+    // let randomThirtytwo = Math.floor(Math.random()*32);
+    // let randomSixtyseven = Math.floor(Math.random()*72);
+
+    // Board-edge Coordinate Randomizer
+    let randomEdge = edgeArray[randomFour];
+
+    // gridEdgeSpawn()
+    //   console.log(randomEdge)
+    //   if edgeArray (randomEdge === top) {
+    //     return 
+    //   }
+
+    // NPC Pokemon Array Random Generator
+    let pokemonArray1 = [
+      <div class="clickable"><img src="img/buneary1.gif"/><p>Normal</p></div>,
+      <div class="clickable"><img src="img/lillipup1.gif"/><p>Normal</p></div>,
+      <div class="clickable"><img src="img/tornadus1.gif"/><p>Flying</p></div>,
+      <div class="clickable"><img src="img/machop1.gif"/><p>Fighting</p></div>,
+      <div class="clickable"><img src="img/cleffa1.gif"/><p>Fairy</p></div>,
+      <div class="clickable"><img src="img/abra1.gif"/><p>Psychic</p></div>,
+      <div class="clickable"><img src="img/duskull1.gif"/><p>Ghost</p></div>,
+      <div class="clickable"><img src="img/zorua1.gif"/><p>Dark</p></div>,
+      <div class="clickable"><img src="img/koffing1.gif"/><p>Poison</p></div>,
+      <div class="clickable"><img src="img/rockruff1.gif"/><p>Rock</p></div>,
+      <div class="clickable"><img src="img/bonsly1.gif"/><p>Rock</p></div>,
+      <div class="clickable"><img src="img/diglett1.gif"/><p>Ground</p></div>,
+      <div class="clickable"><img src="img/klink1.gif"/><p>Steel</p></div>,
+      <div class="clickable"><img src="img/pichu1.gif"/><p>Electric</p></div>,
+      <div class="clickable"><img src="img/kricketot1.gif"/><p>Bug</p></div>,
+      <div class="clickable"><img src="img/treecko1.gif"/><p>Grass</p></div>,
+      <div class="clickable"><img src="img/cyndaquil1.gif"/><p>Fire</p></div>,
+      <div class="clickable"><img src="img/squirtle1.gif"/><p>Water</p></div>,
+      <div class="clickable"><img src="img/snorunt1.gif"/><p>Ice</p></div>,
+      <div class="clickable"><img src="img/axew1.gif"/><p>Dragon</p></div>
+    ]
+    let pokemonArray2 = [
+      <div class="clickable"><img src="img/buneary2.gif"/><p>Normal</p></div>,
+      <div class="clickable"><img src="img/lillipup2.gif"/><p>Normal</p></div>,
+      <div class="clickable"><img src="img/machop2.gif"/><p>Fighting</p></div>,
+      <div class="clickable"><img src="img/cleffa2.gif"/><p>Fairy</p></div>,
+      <div class="clickable"><img src="img/abra2.gif"/><p>Psychic</p></div>,
+      <div class="clickable"><img src="img/duskull2.gif"/><p>Ghost</p></div>,
+      <div class="clickable"><img src="img/klink2.gif"/><p>Steel</p></div>,
+      <div class="clickable"><img src="img/pichu2.gif"/><p>Electric</p></div>,
+      <div class="clickable"><img src="img/treecko2.gif"/><p>Grass</p></div>,
+      <div class="clickable"><img src="img/cyndaquil2.gif"/><p>Fire</p></div>,
+      <div class="clickable"><img src="img/squirtle2.gif"/><p>Water</p></div>,
+      <div class="clickable"><img src="img/snorunt2.gif"/><p>Ice</p></div>,
+      <div class="clickable"><img src="img/axew2.gif"/><p>Dragon</p></div>
+    ]
+    let pokemonArray3 = [
+      <div class="clickable"><img src="img/buneary3.gif"/><p>Normal</p></div>,
+      <div class="clickable"><img src="img/lillipup3.gif"/><p>Normal</p></div>,
+      <div class="clickable"><img src="img/tornadus2.gif"/><p>Flying</p></div>,
+      <div class="clickable"><img src="img/machop3.gif"/><p>Fighting</p></div>,
+      <div class="clickable"><img src="img/cleffa3.gif"/><p>Fairy</p></div>,
+      <div class="clickable"><img src="img/abra3.gif"/><p>Psychic</p></div>,
+      <div class="clickable"><img src="img/duskull3.gif"/><p>Ghost</p></div>,
+      <div class="clickable"><img src="img/zorua2.gif"/><p>Dark</p></div>,
+      <div class="clickable"><img src="img/koffing2.gif"/><p>Poison</p></div>,
+      <div class="clickable"><img src="img/rockruff2a.gif"/><p>Rock</p></div>,
+      <div class="clickable"><img src="img/rockruff2b.gif"/><p>Rock</p></div>,
+      <div class="clickable"><img src="img/bonsly2.gif"/><p>Rock</p></div>,
+      <div class="clickable"><img src="img/diglett2.gif"/><p>Ground</p></div>,
+      <div class="clickable"><img src="img/klink3.gif"/><p>Steel</p></div>,
+      <div class="clickable"><img src="img/pichu3.gif"/><p>Electric</p></div>,
+      <div class="clickable"><img src="img/kricketot2.gif"/><p>Bug</p></div>,
+      <div class="clickable"><img src="img/treecko3.gif"/><p>Grass</p></div>,
+      <div class="clickable"><img src="img/cyndaquil3.gif"/><p>Fire</p></div>,
+      <div class="clickable"><img src="img/squirtle3.gif"/><p>Water</p></div>,
+      <div class="clickable"><img src="img/snorunt3.gif"/><p>Ice</p></div>,
+      <div class="clickable"><img src="img/axew3.gif"/><p>Dragon</p></div>
+    ]
+
+    let pokeCPU1 = pokemonArray1[randomEighteen];
+    let npcSpawn = $(`.r${y}c${x}`).append(`${pokeCPU1}`);
+  }
+}
+
+
+//-----------------------------------------------------
+
+// NPC Movement Styles
+
+
+
+
+//-----------------------------------------------------
+
 // Pokemon Database
 var pokemonSet1 = [ // Pure-type Pokemon
   {name: "Lillipup", type: "normal", img1: <img src="img/lillipup1.gif"/>}, 
@@ -8,7 +104,7 @@ var pokemonSet1 = [ // Pure-type Pokemon
   {name: "Duskull", type: "ghost", img1: <img src="img/duskull1.gif"/>}, 
   {name: "Zorua", type: "dark", img1: <img src="img/zorua1.gif"/>}, 
   {name: "Koffing", type: "poison", img1: <img src="img/koffing1.gif"/>}, 
-  {name: "Bonsly", type: "rock", img1: <img src="img/bonsly1.gif"/>}, 
+  {name: "Rockruff", type: "rock", img1: <img src="img/rockruff1.gif"/>}, 
   {name: "Diglett", type: "ground", img1: <img src="img/diglett1.gif"/>}, 
   {name: "Klink", type: "steel", img1: <img src="img/klink1.gif"/>}, 
   {name: "Pichu", type: "electric", img1: <img src="img/pichu1.gif"/>}, 
@@ -35,9 +131,3 @@ var pokemonSet2 = [ // EVO3 Type-Changers
   {name: "Alolan Vulpix", type: "ice", img1: <img src="img/1.gif"/>}, 
   {name: "Dratini", type: "dragon", img1: <img src="img/1.gif"/>}
 ]
-
-class Spawn {
-  constructor(){
-    // this.cpu1 = $("<img src='img/pichu2.gif'/>")
-  }
-}
