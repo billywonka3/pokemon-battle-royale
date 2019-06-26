9110b2923b826141324f6905e3da3a9d427334a3
Billy Wong <billywonka3@gmail.com>
2019-06-26 17:49:51 -0400

    Add files via upload

--- src/PokemonBR-Game.js
+++ src/PokemonBR-Game.js
@@ -1,21 +1,81 @@
+var pokemonSet1 = [ // Pure-type Pokemon
+  {name: "Buneary", type: "normal", img1: "../img/buneary1.gif", img2: "../img/buneary2.gif", img3: "../img/buneary3.gif"}, 
+  {name: "Tornadus", type: "fly", img1: "../img/tornadus1.gif", img2: "../img/tornadus2.gif", img3: "../img/tornadus3.gif"}, 
+  {name: "Machop", type: "fight", img1: "../img/machop1.gif", img2: "../img/machop2.gif", img3: "../img/machop3.gif"}, 
+  {name: "Cleffa", type: "fairy", img1: "../img/cleffa1.gif", img2: "../img/cleffa2.gif", img3: "../img/cleffa3.gif"}, 
+  {name: "Abra", type: "psychic", img1: "../img/abra1.gif", img2: "../img/abra2.gif", img3: "../img/abra3.gif"}, 
+  {name: "Duskull", type: "ghost", img1: "../img/duskull1.gif", img2: "../img/duskull2.gif", img3: "../img/duskull3.gif"}, 
+  {name: "Zorua", type: "dark", img1: "../img/zorua1.gif", img2: "../img/zorua1.gif"}, 
+  {name: "Ekans", type: "poison", img1: "../img/ekans1.gif", img2: "../img/ekans2.gif"}, 
+  {name: "Rockruff", type: "rock", img1: "../img/rockruff1.gif", img2: "../img/rockruff2.gif"}, 
+  {name: "Diglett", type: "ground", img1: "../img/diglett1.gif", img2: "../img/diglett2.gif"}, 
+  {name: "Klink", type: "steel", img1: "../img/klink1.gif", img2: "../img/klink2.gif", img3: "../img/klink3.gif"}, 
+  {name: "Pichu", type: "electric", img1: "../img/pichu1.gif", img2: "../img/pichu2.gif", img3: "../img/pichu3.gif"}, 
+  {name: "Kricketot", type: "bug", img1: "../img/kricketot1.gif"}, 
+  {name: "Treecko", type: "grass", img1: "../img/treecko1.gif"}, 
+  {name: "Cyndaquil", type: "fire", img1: "../img/cyndaquil1.gif"}, 
+  {name: "Squirtle", type: "water", img1: "../img/squirtle1.gif"}, 
+  {name: "Snorunt", type: "ice", img1: "../img/snorunt1.gif"}, 
+  {name: "Axew", type: "dragon", img1: "../img/axew1.gif"}
+];
+
+class Game {
+  constructor(){
+    this.enemyArr = [];
+    this.player;
+  }
+
+  genPokemon() {
+    let randomPokemon = pokemonSet1[Math.floor(Math.random() * pokemonSet1.length)]
+    this.enemyArr.push(new Pokemon (randPokemon.name, randPokemon.attack, randPokemon.type, randPokemon.img1, Math.floor(Math.random()*12+1), Math.floor(Math.random()*12+1)))
+  }
+
+  createPlayer(imgId) {    
+    let playerPokemon = pokemonSet1[Number(imgId)];
+
+    this.player = new Pokemon(playerPokemon.name, 50, playerPokemon.type, playerPokemon.img1, 1, 1)
+    console.log(playerPokemon);
+  }
+
+  createPokemon() {
+
+    let randomPokemon = pokemonSet1[Math.floor(Math.random() * pokemonSet1.length)];
+    console.log('p ========================== ', randomPokemon);
+    if (Math.floor(Math.random() * 100) % 5 === 0) {
+      this.enemyArr.push(new Pokemon(randomPokemon.name, 10, randomPokemon.type, randomPokemon.img1, Math.floor((Math.random() * 12 )+ 1), Math.floor((Math.random() * 12) + 1)))
+
+    console.log(this.enemyArr);
+
+    let spawnLocation = $(`.r${this.enemyArr.row}c${this.enemyArr.column}`).append(randomPokemon);
+    console.log(spawnLocation)
+    }
+  }
+
+  randomMovement() {
+    for(let i = 0; i < this.pokeArray.length; i++) {
+      // let num1 = this.enemyArr[i].row;
+      // let num2 = this.enemyArr[i].column;
+  
+      if (Math.floor( (Math.random()*200) %5 === 0 ) ){
+        this.enemyArr[i].row += Math.floor(Math.random()*1);
+      } else if ( (Math.floor(Math.random()*200) %5 === 1 ) ){
+        this.enemyArr[i].row -= Math.floor(Math.random()*1);
+      } else if ( (Math.floor(Math.random()*200) %5 === 2 )) {
+        this.enemyArr[i].column += Math.floor(Math.random()*1);
+      } else if ( (Math.floor(Math.random()*200) %5 ===3 )) {
+        this.enemyArr[i].column -= Math.floor(Math.random()*1);
+      }
+    }
+  }
+}
+
+
+//-------------------------------------------------------------------------------
 window.onload = function () {
   // Health & Scoreboard
   let playerHP = document.getElementById("playerHP")
 
   // Instructions Starter Screen
-
-
-  // Starter Pokemon Choice
-  let player = $("#player")
-
-  $(".clickable").click(function() {
-    // this.style.backgroundColor = 'red';
-    let imgSource = $(this).children().prop('src');
-    // console.log(imgSource);
-    $('#player').children().prop('src', imgSource)
-    $(".clickable").empty(); // Remove all other sprites
-    setInterval(npcSpawn, 3000)
-  });
 
   // Player Controls & Movement
   let a = 11;
@@ -27,32 +87,64 @@ window.onload = function () {
       case "ArrowRight": if (b < 22) {b++;} break;
       case "ArrowDown": if (a < 22) {a++;} break;
       case "ArrowLeft": if (b > 11) {b--;} break;
-    }
-    // console.log(`.r${a}c${b}`)
-    $(`.r${a}c${b}`).append(player);
   }
+  // console.log(`.r${a}c${b}`)
+  $(`.r${a}c${b}`).append(player);
+};
 
-  // Collision Function
-  // collision(player) {
-    
-  //   playerHP.value -= 8(); // Use for collision base-damage
+// ---------------------------------------------------------
 
-  // };
+// Starter Pokemon Choice
+let player = $("#player")
+$(".clickable").click(function() {
+  // this.style.backgroundColor = 'red';
+  // let imgSource = $(this).children().prop('src');
 
-}
+  let startGame = new Game;
+  let imgId = $(this).children().prop('id');
+  console.log(startGame);
+
+  startGame.createPlayer(imgId)
+
+  $('#player').empty();
+  $('#player').append($(this).children());
+
+  $(".clickable").empty(); // Remove all other sprites
+  setInterval(startGame.createPokemon, 2000)
+});
+
+// Collision Function
+const dmgArray = [
+  normal =   [1, 1, 1, 1, 1, 0.5, 1, 0, 0.5, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  fighting = [2, 1, 0.5, 0.5, 1, 2, 0.5, 0, 2, 1, 1, 1, 1, 0.5, 2, 1, 2, 0.5], 
+  flying =   [1, 2, 1, 1, 1, 0.5, 2, 1, 0.5, 1, 1, 2, 0.5, 1, 1, 1, 1, 1], 
+  poison =   [1, 1, 1, 0.5, 0.5, 0.5, 1, 0.5, 0, 1, 1, 2, 1, 1, 1, 1, 1, 2], 
+  ground =   [1, 1, 0, 2, 1, 2, 0.5, 1, 2, 2, 1, 0.5, 2, 1, 1, 1, 1, 1], 
+  rock =     [1, 0.5, 2, 1, 0.5, 1, 2, 1, 0.5, 2, 1, 1, 1, 1, 2, 1, 1, 1], 
+  bug =      [1, 0.5, 0.5, 0.5, 1, 1, 1, 0.5, 0.5, 0.5, 1, 2, 1, 2, 1, 1, 2, 0.5], 
+  ghost =    [0, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 0.5, 1], 
+  steel =    [1, 1, 1, 1, 1, 2, 1, 1, 0.5, 0.5, 0.5, 1, 0.5, 1, 2, 1, 1, 2], 
+  fire =     [1, 1, 1, 1, 1, 0.5, 2, 1, 2, 0.5, 0.5, 2, 1, 1, 2, 0.5, 1, 1], 
+  water =    [1, 1, 1, 1, 2, 2, 1, 1, 1, 2, 0.5, 0.5, 1, 1, 1, 0.5, 1, 1], 
+  grass =    [1, 1, 0.5, 0.5, 2, 2, 0.5, 1, 0.5, 0.5, 2, 0.5, 1, 1, 1, 0.5, 1, 1], 
+  electric = [1, 1, 2, 1, 0, 1, 1, 1, 1, 1, 2, 0.5, 0.5, 1, 1, 0.5, 1, 1], 
+  psychic =  [1, 2, 1, 2, 1, 1, 1, 1, 0.5, 1, 1, 1, 1, 0.5, 1, 1, 0, 1], 
+  ice =      [1, 1, 2, 1, 2, 1, 1, 1, 0.5, 0.5, 0.5, 2, 1, 1, 0.5, 2, 1, 1], 
+  dragon =   [1, 1, 1, 1, 1, 1, 1, 1, 0.5, 1, 1, 1, 1, 1, 1, 2, 1, 0],
+  dark =     [1, 0.5, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 0.5, 0.5], 
+  fairy =    [1, 2, 1, 0.5, 1, 1, 1, 1, 0.5, 0.5, 1, 1, 1, 1, 1, 2, 2, 1]
+];
+
+// collision(player) {
+  
+//   playerHP.value -= 8(); // Use for collision base-damage
+
+};
+
 
 //------------------------------------------------------------------------------
 
 // $(`.r${y}c${x}`).find('img').remove() // locate player img within coordinates & remove it
 // $(`.r${y}c${x}`).append(`<img src="img/${pokeCPU}1.gif"/>`) // move in defined direction and add img to current span
 
-
-// class Game {
-//   constructor(){
-    // this.health = document.getElementsById('playerHP').value;
-    // this.score = document.getElementsById('score').value;
-    
-  // }
-  
-
-
+// ------------------------------------------------------------------------
--- src/PokemonBR-Pokemon.js
+++ src/PokemonBR-Pokemon.js
@@ -1,29 +1,77 @@
-// class Spawn {
-//   constructor(){
+class Pokemon {
+  constructor(name, attack, type, img1, row, column) {
+    this.name = name; 
+    this.health = 100; 
+    this.attack = attack; 
+    this.type = type; 
+    this.img1 = img1; 
+    this.row = row;
+    this.column = column;
+  }
+}
+
+// Pokemon Database
+var pokemonSet1 = [ // Pure-type Pokemon
+  {name: "Buneary", type: "normal", img1: "../img/buneary1.gif", img2: "../img/buneary2.gif", img3: "../img/buneary3.gif"}, 
+  {name: "Tornadus", type: "fly", img1: "../img/tornadus1.gif", img2: "../img/tornadus2.gif", img3: "../img/tornadus3.gif"}, 
+  {name: "Machop", type: "fight", img1: "../img/machop1.gif", img2: "../img/machop2.gif", img3: "../img/machop3.gif"}, 
+  {name: "Cleffa", type: "fairy", img1: "../img/cleffa1.gif", img2: "../img/cleffa2.gif", img3: "../img/cleffa3.gif"}, 
+  {name: "Abra", type: "psychic", img1: "../img/abra1.gif", img2: "../img/abra2.gif", img3: "../img/abra3.gif"}, 
+  {name: "Duskull", type: "ghost", img1: "../img/duskull1.gif", img2: "../img/duskull2.gif", img3: "../img/duskull3.gif"}, 
+  {name: "Zorua", type: "dark", img1: "../img/zorua1.gif", img2: "../img/zorua1.gif"}, 
+  {name: "Ekans", type: "poison", img1: "../img/ekans1.gif", img2: "../img/ekans2.gif"}, 
+  {name: "Rockruff", type: "rock", img1: "../img/rockruff1.gif", img2: "../img/rockruff2.gif"}, 
+  {name: "Diglett", type: "ground", img1: "../img/diglett1.gif", img2: "../img/diglett2.gif"}, 
+  {name: "Klink", type: "steel", img1: "../img/klink1.gif", img2: "../img/klink2.gif", img3: "../img/klink3.gif"}, 
+  {name: "Pichu", type: "electric", img1: "../img/pichu1.gif", img2: "../img/pichu2.gif", img3: "../img/pichu3.gif"}, 
+  {name: "Kricketot", type: "bug", img1: "../img/kricketot1.gif"}, 
+  {name: "Treecko", type: "grass", img1: "../img/treecko1.gif"}, 
+  {name: "Cyndaquil", type: "fire", img1: "../img/cyndaquil1.gif"}, 
+  {name: "Squirtle", type: "water", img1: "../img/squirtle1.gif"}, 
+  {name: "Snorunt", type: "ice", img1: "../img/snorunt1.gif"}, 
+  {name: "Axew", type: "dragon", img1: "../img/axew1.gif"}
+];
+
+// var pokemonSet2 = [ // EVO3 Type-Changers
+//   {name: "Bunnelby", type: "normal", img1: img/1.gif}, 
+//   {name: "Pancham", type: "fight", img1: img/1.gif}, 
+//   {name: "Togepi", type: "fairy", img1: img/1.gif}, 
+//   {name: "Nidoran", type: "poison", img1: img/1.gif}, 
+//   {name: "Onix", type: "rock", img1: img/1.gif}, 
+//   {name: "Cubone", type: "ground", img1: img/1.gif}, 
+//   {name: "Alolan Pichu", type: "", img1: img/1.gif}, 
+//   {name: "Caterpee", type: "bug", img1: img/1.gif}, 
+//   {name: "Turtwig", type: "grass", img1: img/1.gif}, 
+//   {name: "Charmander", type: "fire", img: img/1.gif}, 
+//   {name: "Staryu", type: "water", img1: img/1.gif}, 
+//   {name: "Alolan Vulpix", type: "ice", img1: img/1.gif}, 
+//   {name: "Dratini", type: "dragon", img1: img/1.gif}
+// ];
+
+// ------------------------------------------------------------------------
+// ------------------------------------------------------------------------
   function npcSpawn() {
-    // NPC Pokemon Arrays
+    // Pokemon Div Database
+    // `<div class="npc"><img id="${i}" src="${enemyArr[i].img1}"/><p>${${enemyArr[i].type}}</p></div>`
     let pokemonArray1 = [
-      '<div class="npc"><img src="img/buneary1.gif"/><p>Normal</p></div>',
-      '<div class="npc"><img src="img/lillipup1.gif"/><p>Normal</p></div>',
-      '<div class="npc"><img src="img/tornadus1.gif"/><p>Flying</p></div>',
-      '<div class="npc"><img src="img/machop1.gif"/><p>Fighting</p></div>',
-      '<div class="npc"><img src="img/cleffa1.gif"/><p>Fairy</p></div>',
-      '<div class="npc"><img src="img/abra1.gif"/><p>Psychic</p></div>',
-      '<div class="npc"><img src="img/duskull1.gif"/><p>Ghost</p></div>',
-      '<div class="npc"><img src="img/zorua1.gif"/><p>Dark</p></div>',
-      '<div class="npc"><img src="img/grimer1.gif"/><p>Poison</p></div>',
-      '<div class="npc"><img src="img/ekans1.gif"/><p>Poison</p></div>',
-      '<div class="npc"><img src="img/rockruff1.gif"/><p>Rock</p></div>',
-      '<div class="npc"><img src="img/bonsly1.gif"/><p>Rock</p></div>',
-      '<div class="npc"><img src="img/diglett1.gif"/><p>Ground</p></div>',
-      '<div class="npc"><img src="img/klink1.gif"/><p>Steel</p></div>',
-      '<div class="npc"><img src="img/pichu1.gif"/><p>Electric</p></div>',
-      '<div class="npc"><img src="img/kricketot1.gif"/><p>Bug</p></div>',
-      '<div class="npc"><img src="img/treecko1.gif"/><p>Grass</p></div>',
-      '<div class="npc"><img src="img/cyndaquil1.gif"/><p>Fire</p></div>',
-      '<div class="npc"><img src="img/squirtle1.gif"/><p>Water</p></div>',
-      '<div class="npc"><img src="img/snorunt1.gif"/><p>Ice</p></div>',
-      '<div class="npc"><img src="img/axew1.gif"/><p>Dragon</p></div>'];
+      '<div class="npc"><img id="0" src="img/buneary1.gif"/><p>Normal</p></div>',
+      '<div class="npc"><img id="1" src="img/tornadus1.gif"/><p>Flying</p></div>',
+      '<div class="npc"><img id="2" src="img/machop1.gif"/><p>Fighting</p></div>',
+      '<div class="npc"><img id="3" src="img/cleffa1.gif"/><p>Fairy</p></div>',
+      '<div class="npc"><img id="4" src="img/abra1.gif"/><p>Psychic</p></div>',
+      '<div class="npc"><img id="5" src="img/duskull1.gif"/><p>Ghost</p></div>',
+      '<div class="npc"><img id="6" src="img/zorua1.gif"/><p>Dark</p></div>',
+      '<div class="npc"><img id="7" src="img/grimer1.gif"/><p>Poison</p></div>',
+      '<div class="npc"><img id="8" src="img/rockruff1.gif"/><p>Rock</p></div>',
+      '<div class="npc"><img id="9" src="img/diglett1.gif"/><p>Ground</p></div>',
+      '<div class="npc"><img id="10" src="img/klink1.gif"/><p>Steel</p></div>',
+      '<div class="npc"><img id="11" src="img/pichu1.gif"/><p>Electric</p></div>',
+      '<div class="npc"><img id="12" src="img/kricketot1.gif"/><p>Bug</p></div>',
+      '<div class="npc"><img id="13" src="img/treecko1.gif"/><p>Grass</p></div>',
+      '<div class="npc"><img id="14" src="img/cyndaquil1.gif"/><p>Fire</p></div>',
+      '<div class="npc"><img id="15" src="img/squirtle1.gif"/><p>Water</p></div>',
+      '<div class="npc"><img id="16" src="img/snorunt1.gif"/><p>Ice</p></div>',
+      '<div class="npc"><img id="17" src="img/axew1.gif"/><p>Dragon</p></div>'];
 
     // let pokemonArray2 = [
     //   <div class="npc"><img src="img/buneary2.gif"/><p>Normal</p></div>,
@@ -68,33 +116,28 @@
     //   <div class="npc"><img src="img/snorunt3.gif"/><p>Ice</p></div>,
     //   <div class="npc"><img src="img/axew3.gif"/><p>Dragon</p></div>];
 
-    let coordinateArray = ["r11c11", "r11c12", "r11c13", "r11c14", "r11c15", "r11c16", "r11c17", "r11c18", "r11c19", "r11c20", "r11c21", "r11c22", 
-                          "r12c22", "r13c22", "r14c22", "r15c22", "r16c22", "r17c22", "r18c22", "r19c22", "r20c22", "r21c22", "r22c22",
-                          "r22c11", "r22c12", "r22c13", "r22c14", "r22c15", "r22c16", "r22c17", "r22c18", "r22c19", "r22c20", "r22c21",
-                          "r12c11", "r13c11", "r14c11", "r15c11", "r16c11", "r17c11", "r18c11", "r19c11", "r20c11", "r21c11"];
-
   // Random Number Generators
     // let randomFour = Math.floor(Math.random()*4);
-    // let randomTwelve = Math.floor(Math.random()*12);
-    let randomTwentyone = Math.floor(Math.random()*21);
-    let randomFortyfour = Math.floor(Math.random()*44);
+    // let randomTwelve = Math.floor(Math.random()*12+1);
+    // let randomTwentyone = Math.floor(Math.random()*21);
+    // let randomFortyfour = Math.floor(Math.random()*44);
     // let randomSixtyseven = Math.floor(Math.random()*72);
 
   // Random Spawning along grid edges
-    let randomEdge = coordinateArray[randomFortyfour];
-    console.log(randomEdge);
-    let pokeCPU1 = pokemonArray1[randomTwentyone];
-    console.log(pokeCPU1);
-    let spawnRate = $(`.${randomEdge}`).append(`${pokeCPU1}`);
+    // let randomEdge = coordinateArray[randomFortyfour];
+    // console.log(randomEdge);
+    // let pokeCPU1 = pokemonArray1[randomTwentyone];
+    // console.log(pokeCPU1);
+    // let spawnRate = $(`.${randomEdge}`).append(`${pokeCPU1}`);
 
-    if (randomEdge == "r11c12" || "r11c13" || "r11c14" || "r11c15" || "r11c16" || "r11c17" || "r11c18" || "r11c19" || "r11c20" || "r11c21") {
-      moveDown(pokeCPU1, randomEdge); // moving down from row 11
-    } else if (randomEdge == "r12c22" || "r13c22" || "r14c22" || "r15c22" || "r16c22" || "r17c22" || "r18c22" || "r19c22"|| "r20c22" || "r21c22") {
-      moveLeft (pokeCPU1, randomEdge); // moving left from column 22
-    } else if (randomEdge == "r22c12" || "r22c13" || "r22c14" || "r22c15" || "r22c16" || "r22c17" || "r22c18" || "r22c19" || "r22c20" || "r22c21") {
-      moveUp (pokeCPU1, randomEdge); // moving up from row 22
-    } else if (randomEdge == "r02c11" || "r13c11" || "r14c11" || "r15c11" || "r16c11" || "r17c11" || "r18c11" || "r19c11" || "r20c11" || "r21c11") {
-      moveRight (pokeCPU1, randomEdge); // moving right from column 11
+    // if (randomEdge == "r11c12" || "r11c13" || "r11c14" || "r11c15" || "r11c16" || "r11c17" || "r11c18" || "r11c19" || "r11c20" || "r11c21") {
+    //   moveDown(pokeCPU1, randomEdge); // moving down from row 11
+    // } else if (randomEdge == "r12c22" || "r13c22" || "r14c22" || "r15c22" || "r16c22" || "r17c22" || "r18c22" || "r19c22"|| "r20c22" || "r21c22") {
+    //   moveLeft (pokeCPU1, randomEdge); // moving left from column 22
+    // } else if (randomEdge == "r22c12" || "r22c13" || "r22c14" || "r22c15" || "r22c16" || "r22c17" || "r22c18" || "r22c19" || "r22c20" || "r22c21") {
+    //   moveUp (pokeCPU1, randomEdge); // moving up from row 22
+    // } else if (randomEdge == "r02c11" || "r13c11" || "r14c11" || "r15c11" || "r16c11" || "r17c11" || "r18c11" || "r19c11" || "r20c11" || "r21c11") {
+    //   moveRight (pokeCPU1, randomEdge); // moving right from column 11
 
     // } else if (randomEdge == "r11c11" || "r11c12") {
     //   moveDiaDR(pokeCPU1, randomEdge);  // moving diagonal from top-left corners
@@ -122,134 +165,108 @@
     //   moveCornerKnightUR(pokeCPU1, randomEdge);  // moving in knight pattern (2L-1U) from bottom-right corners
     // }
     
-    return spawnRate;
-   };
-  }
+    // return spawnRate;
+};
+
 
 //-----------------------------------------------------
 
 // NPC Movement Styles
-function moveDown (who, where) {
-  let x = Number(where.substring(1, 3));
-  let y = Number(where.substring(4, 6));
-  console.log('down', x, y)
+// function randomMove (who, where) {
 
-  $(`.r${x}c${y}`).empty()
+// }
 
-  x++;
-  console.log('down', x, y)
+// function moveDown (who, where) {
+//   let x = Number(where.substring(1, 3));
+//   let y = Number(where.substring(4, 6));
+//   console.log('down', x, y)
 
-  $(`.r${x}c${y}`).append(who)
+//   $(`.r${x}c${y}`).empty()
 
-  setTimeout(()=> {
+//   x++;
+//   console.log('down', x, y)
 
-    let newPlace = `r${x}c${y}`
+//   $(`.r${x}c${y}`).append(who)
 
-    moveDown(who, newPlace)
+//   setTimeout(()=> {
 
-  }, 1000)
-};
+//     let newPlace = `r${x}c${y}`
 
-function moveLeft () {
-  let x = Number(where.substring(1, 3));
-  let y = Number(where.substring(4, 6));
-  console.log('left', x, y)
+//     moveDown(who, newPlace)
 
-  $(`.r${x}c${y}`).empty()
+//   }, 1000)
+// };
 
-  y--;
-  console.log('left', x, y)
+// function moveLeft () {
+//   let x = Number(where.substring(1, 3));
+//   let y = Number(where.substring(4, 6));
+//   console.log('left', x, y)
 
-  $(`.r${x}c${y}`).append(who)
+//   $(`.r${x}c${y}`).empty()
 
-  setTimeout(()=> {
+//   y--;
+//   console.log('left', x, y)
 
-    let newPlace = `r${x}c${y}`
+//   $(`.r${x}c${y}`).append(who)
 
-    moveLeft(who, newPlace)
+//   setTimeout(()=> {
 
-  }, 1000)
-};
+//     let newPlace = `r${x}c${y}`
 
-function moveUp (who) {
-  let x = Number(where.substring(1, 3));
-  let y = Number(where.substring(4, 6));
-  console.log('up', x, y)
+//     moveLeft(who, newPlace)
 
-  $(`.r${x}c${y}`).empty()
+//   }, 1000)
+// };
 
-  x--;
-  console.log('up', x, y)
+// function moveUp (who) {
+//   let x = Number(where.substring(1, 3));
+//   let y = Number(where.substring(4, 6));
+//   console.log('up', x, y)
+
+//   $(`.r${x}c${y}`).empty()
+
+//   x--;
+//   console.log('up', x, y)
   
-  $(`.r${x}c${y}`).append(who)
+//   $(`.r${x}c${y}`).append(who)
 
-  setTimeout(()=> {
+//   setTimeout(()=> {
 
-    let newPlace = `r${x}c${y}`
+//     let newPlace = `r${x}c${y}`
 
-    moveUp(who, newPlace)
+//     moveUp(who, newPlace)
 
-  }, 1000)
-};
+//   }, 1000)
+// };
 
-function moveRight () {
-  let x = Number(where.substring(1, 3));
-  let y = Number(where.substring(4, 6));
-  console.log('right', x, y)
+// function moveRight () {
+//   let x = Number(where.substring(1, 3));
+//   let y = Number(where.substring(4, 6));
+//   console.log('right', x, y)
 
-  $(`.r${x}c${y}`).empty()
+//   $(`.r${x}c${y}`).empty()
   
-  y++;
-  console.log('right', x, y)
+//   y++;
+//   console.log('right', x, y)
 
-  $(`.r${x}c${y}`).append(who)
+//   $(`.r${x}c${y}`).append(who)
 
-  setTimeout(()=> {
+//   setTimeout(()=> {
 
-    let newPlace = `r${x}c${y}`
+//     let newPlace = `r${x}c${y}`
 
-    moveRight(who, newPlace)
+//     moveRight(who, newPlace)
 
-  }, 1000)
-};
+//   }, 1000)
+// };
 
 
 //-----------------------------------------------------
 
-// Player Pokemon Database
-// var pokemonSet1 = [ // Pure-type Pokemon
-//   {name: "Lillipup", type: "normal", img1: <img src="img/lillipup1.gif"/>}, 
-//   {name: "Tornadus", type: "fly", img1: <img src="img/tornadus1.gif"/>}, 
-//   {name: "Machop", type: "fight", img1: <img src="img/machop1.gif"/>}, 
-//   {name: "Cleffa", type: "fairy", img1: <img src="img/cleffa1.gif"/>}, 
-//   {name: "Abra", type: "psychic", img1: <img src="img/abra1.gif"/>}, 
-//   {name: "Duskull", type: "ghost", img1: <img src="img/duskull1.gif"/>}, 
-//   {name: "Zorua", type: "dark", img1: <img src="img/zorua1.gif"/>}, 
-//   {name: "Koffing", type: "poison", img1: <img src="img/koffing1.gif"/>}, 
-//   {name: "Rockruff", type: "rock", img1: <img src="img/rockruff1.gif"/>}, 
-//   {name: "Diglett", type: "ground", img1: <img src="img/diglett1.gif"/>}, 
-//   {name: "Klink", type: "steel", img1: <img src="img/klink1.gif"/>}, 
-//   {name: "Pichu", type: "electric", img1: <img src="img/pichu1.gif"/>}, 
-//   {name: "Kricketot", type: "bug", img1: <img src="img/kricketot1.gif"/>}, 
-//   {name: "Treecko", type: "grass", img1: <img src="img/treecko1.gif"/>}, 
-//   {name: "Cyndaquil", type: "fire", img1: <img src="img/cyndaquil1.gif"/>}, 
-//   {name: "Squirtle", type: "water", img1: <img src="img/squirtle1.gif"/>}, 
-//   {name: "Snorunt", type: "ice", img1: <img src="img/snorunt1.gif"/>}, 
-//   {name: "Axew", type: "dragon", img1: <img src="img/axew1.gif"/>}
-// ];
-
-// var pokemonSet2 = [ // EVO3 Type-Changers
-//   {name: "Bunnelby", type: "normal", img1: <img src="img/1.gif"/>}, 
-//   {name: "Pancham", type: "fight", img1: <img src="img/1.gif"/>}, 
-//   {name: "Togepi", type: "fairy", img1: <img src="img/1.gif"/>}, 
-//   {name: "Nidoran", type: "poison", img1: <img src="img/1.gif"/>}, 
-//   {name: "Onix", type: "rock", img1: <img src="img/1.gif"/>}, 
-//   {name: "Cubone", type: "ground", img1: <img src="img/1.gif"/>}, 
-//   {name: "Alolan Pichu", type: "", img1: <img src="img/1.gif"/>}, 
-//   {name: "Caterpee", type: "bug", img1: <img src="img/1.gif"/>}, 
-//   {name: "Turtwig", type: "grass", img1: <img src="img/1.gif"/>}, 
-//   {name: "Charmander", type: "fire", img: <img src="img/1.gif"/>}, 
-//   {name: "Staryu", type: "water", img1: <img src="img/1.gif"/>}, 
-//   {name: "Alolan Vulpix", type: "ice", img1: <img src="img/1.gif"/>}, 
-//   {name: "Dratini", type: "dragon", img1: <img src="img/1.gif"/>}
-// ];
+// Pokemon Object Array Additions
+// {name: "Lillipup", type: "normal", img1: img/lillipup1.gif, img2: img/lillipup2.gif, img3: img/lillipup3.gif}, 
+// '<div class="npc"><img id="1" src="img/lillipup1.gif"/><p>Normal</p></div>',
+// {name: "Grimer", type: "poison", img1: img/grimer1.gif, img2: img/grimer2.gif}, 
+// '<div class="npc"><img id="9" src="img/ekans1.gif"/><p>Poison</p></div>',
+// {name: "Bonsly", type: "rock", img1: img/bonsly1.gif, img2: img/bonsly2.gif}, 
+// '<div class="npc"><img id="11" src="img/bonsly1.gif"/><p>Rock</p></div>',
